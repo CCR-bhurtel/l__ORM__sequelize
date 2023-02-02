8aa6ef7f7398bbfb99ccd85dd54c1318de1c1841
cceer <sisir.sital@gmail.com>
2023-02-02 18:05:54 +0545

    new branch initialized

--- association.js
+++ association.js
@@ -51,7 +51,7 @@ Weather.init(
     { sequelize, freezeTableName: true, tableName: 'weather' }
 );
 
-City.hasMany(Weather, { onDelete: 'RESTRICT', foreignKey: { name: 'city_id' } });
+City.hasMany(Weather, { onDelete: 'RESTRICT' });
 Weather.belongsTo(City, { onUpdate: 'CASCADE', foreignKey: { name: 'city_id' } });
 
 const connectAndSyncDb = async () => {
@@ -65,98 +65,8 @@ const connectAndSyncDb = async () => {
         sequelize.close();
         process.exit(1);
     }
-    const butwal = await City.findOne({
-        where: {
-            name: 'Butwal',
-        },
-        include: [
-            {
-                model: Weather,
-                where: {
-                    maxTemp: { [Op.gt]: 12 },
-                },
-            },
-        ],
-    });
-    // await City.restore({
-    //     where: {
-    //         name: 'Dang',
-    //     },
-    // });
-    console.log(butwal.toJSON());
-    const totalWeatherRecordsOfButwal = await butwal.countWeather();
-    // console.log(totalWeatherRecordsOfButwal);
-    // await butwal.createWeather({ maxTemp: 20, minTemp: 12, date: new Date('2023-2-1') });
-    // const weather = await Weather.findOne({
-    //     where: {
-    //         id: 1,
-    //     },
-    //     include: 'City',
-    // });
-
-    // console.log(weather.toJSON());
-
-    // await butwal.setWeather(weather1);
-
-    // await butwal.setWeather(weather2);
-    // const city = await City.findOne({
-    //     where: {
-    //         name: 'Butwal',
-    //     },
-    //     include: Weather,
-    // });
 };
 
 (async () => {
     await connectAndSyncDb();
-
-    // const cities = await City.bulkCreate([
-    //     { name: 'Kathmandu', population: '2345643' },
-    //     { name: 'Pokhara', population: 345333 },
-    //     { name: 'Butwal', population: 294564 },
-    //     { name: 'Chitwan', population: 234553 },
-    //     { name: 'Morang', population: 96724 },
-    //     { name: 'Birgunj', population: 654343 },
-    //     { name: 'Dang', population: 34643 },
-    // ]);
-    // const weathers = await Weather.bulkCreate([
-    //     { maxTemp: 12, minTemp: 3, date: new Date('2023-1-27') },
-    //     { maxTemp: 20, minTemp: 12, date: new Date('2023-1-27') },
-    //     { maxTemp: 23, minTemp: 15, date: new Date('2023-1-27') },
-    //     { maxTemp: 21, minTemp: 10, date: new Date('2023-1-27') },
-    //     { maxTemp: 25, minTemp: 12, date: new Date('2023-1-27') },
-    //     { maxTemp: 26, minTemp: 10, date: new Date('2023-1-27') },
-    //     { maxTemp: 16, minTemp: 3, date: new Date('2023-1-28') },
-    //     { maxTemp: 18, minTemp: 8, date: new Date('2023-1-28') },
-    //     { maxTemp: 26, minTemp: 7, date: new Date('2023-1-28') },
-    //     { maxTemp: 24, minTemp: 13, date: new Date('2023-1-28') },
-    // ]);
-
-    // -----------------------Lazy Loading --------------
-    // const city = await City.findOne({
-    //     where: {
-    //         name: 'Butwal',
-    //     },
-    // });
-    // console.log(city.name);
-    // const weather = await city.getWeather();
-    // console.log(weather.maxTemp);
-
-    // ----------------------Eager Loading------------------
-    // const cityWithWeather = await City.findOne({
-    //     where: {
-    //         name: 'Butwal',
-    //     },
-    //     include: 'Weather',
-    // });
-
-    // using association for key other than primary
-    /*
-belongsto relationship:
-    Bar.belongsTo(Foo, {target:"name", foreignKey:"fooName"})
-
-hasOne and hasMany relationship
-    Foo.hasOne(Bar, { sourceKey: 'name', foreignKey: 'fooName' });
-    Bar.hasMany(Baz, { sourceKey: 'title', foreignKey: 'barTitle' });
-*/
 })();
